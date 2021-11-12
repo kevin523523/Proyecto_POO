b9bfc72802c225abfe2300a9cc1309ddaac8bc2b
RamsesGarcia <ramigarc@espol.edu.ec>
2021-11-12 18:10:28 -0500

    Creacion del proyecto

--- SistemaFacturacion/src/main/java/Medidores/Lectura.java
+++ SistemaFacturacion/src/main/java/Medidores/Lectura.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Interfaz;
+package Medidores;
 
 /**
  *
  * @author Ramses
  */
-public class Usuarios {
-    private String nombre;
-    private String contraseña;
+public class Lectura {
+    
 }
--- SistemaFacturacion/src/main/java/Usuario/NewClass.java
+++ SistemaFacturacion/src/main/java/Usuario/NewClass.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Interfaz;
+package Usuario;
 
 /**
  *
  * @author Ramses
  */
-public class Abonados {
-    private String cedula;
-
+public class NewClass {
+    
 }
