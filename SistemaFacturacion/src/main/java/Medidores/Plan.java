9374dc6a2fd724962dca6a98cccf2582c79453c5
RamsesGarcia <ramigarc@espol.edu.ec>
2021-11-12 19:15:05 -0500

    Creacion de claases

--- SistemaFacturacion/src/main/java/Medidores/Plan.java
+++ SistemaFacturacion/src/main/java/Medidores/Plan.java
@@ -3,11 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Medidores;
+import Medidores.Provincia;
+import java.util.List;
 
 /**
  *
  * @author Ramses
  */
 public class Plan {
-    
+    private String nombre;
+    private Double costoKilovatio;
+    private List<HorarioPico> picos;
+    private List<Provincia> provincia; 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getCostoKilovatio() {
+        return costoKilovatio;
+    }
+
+    public void setCostoKilovatio(Double costoKilovatio) {
+        this.costoKilovatio = costoKilovatio;
+    }
+   
 }
